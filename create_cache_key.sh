35fdd91f0f41523a3480810e90f8e7a000f457fb
awalsh128 <awalsh128@gomail.com>
2021-10-21 15:54:29 -0700

    Add more messaging to scripts.

--- create_cache_key.sh
+++ create_cache_key.sh
@@ -6,16 +6,18 @@ set -e
 version=$1
 packages=${@:2}
 
-echo "* Creating cache key..."
+echo "::group::Create Cache Key"
 
 # Remove package delimiters, sort (requires newline) and then convert back to inline list.
 normalized_list=$(echo $packages | sed 's/[\s,]+/ /g' | tr ' ' '\n' | sort | tr '\n' ' ')
-echo "* Normalized package list is '$normalized_list'."
+echo "::debug::Normalized package list is '$normalized_list'."
 
 value=$(echo $normalized_list @ $version)
-echo "* Value to hash is '$value'."
+echo "::debug::Value to hash is '$value'."
 
 key=$(echo $value | md5sum | /bin/cut -f1 -d' ')
-echo "* Value hashed as '$key'."
+echo "::debug::Value hashed as '$key'."
+
+echo "::endgroup::"
 
 echo "CACHE_KEY=$key" >> $GITHUB_ENV
