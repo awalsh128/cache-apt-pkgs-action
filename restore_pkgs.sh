dca3d33521d77babff5049d12cf48101e77b80ff
awalsh128 <awalsh128@gomail.com>
2021-10-21 22:42:51 -0700

    Get rid of package variables.

--- restore_pkgs.sh
+++ restore_pkgs.sh
@@ -16,18 +16,18 @@ for cache_filepath in $cache_filepaths; do
   echo "- $(basename $cache_filepath)"
 done
 
+echo "Reading from manifest..."
+for logline in $(cat $cache_dir/manifest.log | tr ',' '\n' ); do
+  echo "- $(echo $logline | tr ',' ' ')"
+done
+
 # Only search for archived results. Manifest and cache key also live here.
 cache_pkg_filepaths=$(ls -1 $cache_dir/*.tar.gz | sort)
 cache_pkg_filecount=$(echo $cache_pkg_filepaths | wc -w)
-echo "Found $cache_pkg_filecount packages in the cache."
-for cache_pkg_filepath in $cache_pkg_filepaths; do
-  echo "- $(basename $cache_pkg_filepath)"
-done
-
 echo "Restoring $cache_pkg_filecount packages from cache..."
-for cache_pkg_filepath in $cache_pkg_filepaths; do 
-  package=$(basename $cache_pkg_filepath | awk -F. '{print $1}')
-  echo "- $package"
+for cache_pkg_filepath in $cache_pkg_filepaths; do
+  echo -n "- $(basename $cache_pkg_filepath) restoring..."
   sudo tar -xf $cache_pkg_filepath -C $cache_restore_root > /dev/null
+  echo "done."
 done
 echo "done."
