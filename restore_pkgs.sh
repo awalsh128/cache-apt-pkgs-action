9ca1c60417b9c8c4b75c9f4412d01104318a2874
awalsh128 <awalsh128@gomail.com>
2021-10-21 22:15:05 -0700

    Add more reporting to restore phase.

--- restore_pkgs.sh
+++ restore_pkgs.sh
@@ -13,14 +13,23 @@ cache_restore_root=$2
 # List of the packages to use.
 packages="${@:3}"
 
-# Only search for archived results. Manifest and cache key also live here.
-cache_filenames=$(ls -1 $cache_dir | grep .tar.gz | sort)
-cache_filename_count=$(echo $cache_filenames | wc -w)
-
-echo -n "Restoring $cache_filename_count packages from cache..."
+cache_filenames=$(ls -1 $cache_dir | sort)
+echo "Found $(echo $cache_filenames | wc -w) files in the cache."
 for cache_filename in $cache_filenames; do
-  cache_filepath=$cache_dir/$package.tar.gz
-  echo "- $package ($cache_filepath)"
-  sudo tar -xf $cache_filepath -C $cache_restore_root > /dev/null
+  echo "- $(basename $cache_filename)"
+done
+
+# Only search for archived results. Manifest and cache key also live here.
+cache_pkg_filenames=$(ls -1 $cache_dir/*.tar.gz | sort)
+echo "Found $(echo $cache_filenames | wc -w) packages in the cache."
+for cache_pkg_filename in $cache_pkg_filenames; do
+  echo "- $(basename $cache_pkg_filename)"
+done
+
+echo "Restoring $cache_filename_count packages from cache..."
+for cache_pkg_filename in $cache_pkg_filenames; do
+  cache_pkg_filepath=$cache_dir/$package.tar.gz
+  echo "- $package ($(basename $cache_pkg_filepath))"
+  sudo tar -xf $cache_pkg_filepath -C $cache_restore_root > /dev/null
 done
 echo "done."
