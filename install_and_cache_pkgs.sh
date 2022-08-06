6e061b91428432839782ad4bf6d46c5f9dcc0f95
awalsh128 <awalsh128@gomail.com>
2022-08-05 23:42:36 -0700

    Minor formatting change and whitespace removal.

--- install_and_cache_pkgs.sh
+++ install_and_cache_pkgs.sh
@@ -75,8 +75,8 @@ for installed_package in ${installed_packages}; do
       while IFS= read -r f; do     
         if test -f $f || test -L $f; then echo "${f:1}"; fi;  #${f:1} removes the leading slash that Tar disallows
       done |
-      xargs tar -czf "${cache_filepath}" -C /      
-    log "    done (compressed size $(du -h "${cache_filepath}" | cut -f1))."
+      xargs tar -czf "${cache_filepath}" -C /
+    log "  * Cached ${installed_package_name} to ${cache_filepath} (compressed size $(du -h "${cache_filepath}" | cut -f1))."
   fi
 
   # Comma delimited name:ver pairs in the all packages manifest.
