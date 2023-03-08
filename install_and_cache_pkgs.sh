bdc09286d197e8fc550e850123762c10eacc156f
Pascal Roeleven <dev@pascalroeleven.nl>
2023-03-08 18:04:22 +0100

    Only install apt-fast from source if not present (#97)
    * Revert "Don't install apt-fast from source (#96)"
    
    This reverts commit 854bb539e1ea0eaf0d146dcc1e15197cc2758970.
    
    * Only install apt-fast from source if not present

--- install_and_cache_pkgs.sh
+++ install_and_cache_pkgs.sh
@@ -37,6 +37,15 @@ write_manifest "main" "${manifest_main}" "${cache_dir}/manifest_main.log"
 
 log_empty_line
 
+if ! apt-fast --version > /dev/null 2>&1; then
+  log "Installing apt-fast for optimized installs..."
+  # Install apt-fast for optimized installs.
+  /bin/bash -c "$(curl -sL https://git.io/vokNn)"
+  log "done"
+
+  log_empty_line
+fi
+
 log "Updating APT package list..."
 if [[ -z "$(find -H /var/lib/apt/lists -maxdepth 0 -mmin -5)" ]]; then
   sudo apt-fast update > /dev/null
